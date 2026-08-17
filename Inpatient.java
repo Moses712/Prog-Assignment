547b72d7acb08bb233092d10a047583026dcab4f
Moses712 <ST10533076@rcconnect.edu.za>
2026-08-17 20:26:39 +0200

    Create Inpatient.java

---
+++ Inpatient.java
@@ -0,0 +1,23 @@
+public class Inpatient extends Patient {
+    private String wardNumber;
+    private String bedNumber;
+
+    public Inpatient(String patientId, String firstName, String lastName, int age, String gender, String medicalCondition, String wardNumber, String bedNumber) {
+        super(patientId, firstName, lastName, age, gender, medicalCondition, PatientCategory.INPATIENT);
+        this.wardNumber = wardNumber;
+        this.bedNumber = bedNumber;
+    }
+
+    public String getWardNumber() { return wardNumber; }
+    public void setWardNumber(String wardNumber) { this.wardNumber = wardNumber; }
+
+    public String getBedNumber() { return bedNumber; }
+    public void setBedNumber(String bedNumber) { this.bedNumber = bedNumber; }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("   -> Ward Number: " + (wardNumber != null ? wardNumber : "Unassigned") + 
+                           " | Bed Number: " + (bedNumber != null ? bedNumber : "Unassigned"));
+    }
+}
